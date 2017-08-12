6f01c437a90af398a8a70898063c658812666a2f
usama-ayub <usamaayub2012@gmail.com>
2017-08-12 08:05:23 -0700

    basic server setup add

---
+++ config/db.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+const url = `mongodb://admin:admin@ds129610.mlab.com:29610/test-db`;
+
+export default () => {
+    mongoose.Promise = global.Promise;
+    mongoose.connect(url);
+    mongoose.connection
+        .once('open', () => console.log('mongodb run'))
+        .on('err', err => console.error(err));
+}
---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dbConfig from '../config/db';
+
+const app = express();
+
+// Database
+dbConfig();
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, err => {
+    if (err) {
+        console.log(err);
+    } {
+        console.log(`App listen to port: ${PORT}`);
+    }
+});
